cc5dd374aba8e320ae83e4b0ba7cd2d6598762d2
FatmaAlamri9 <141577087+FatmaAlamri9@users.noreply.github.com>
2023-08-12 16:00:25 +0400

    Add files via upload

---
+++ problemSolving/Q5/Q5.js
@@ -0,0 +1,23 @@
+//Q5
+
+const num=[1,2,2,8];
+const den=[2,4,6,12];
+
+
+if((n1= num[0]/den[0] + num[1]/den[1])==1)
+    {
+
+console.log("result="+n1)
+    }
+
+else if((n2=num[2]/den[2] + num[3]/den[3]) ==1)
+{
+    console.log("result="+n2)
+}
+
+else{
+    console.log("try other numbers")
+}
+
+
+
