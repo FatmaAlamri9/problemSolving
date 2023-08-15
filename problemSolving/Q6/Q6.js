f740cfd41f79c9e6ccf15b3f6a20bdd97f06b6be
FatmaAlamri9 <141577087+FatmaAlamri9@users.noreply.github.com>
2023-08-15 11:54:58 +0400

    Add files via upload

---
+++ problemSolving/Q6/Q6.js
@@ -0,0 +1,19 @@
+//Q6:
+
+const dis = (numx,numy) => {
+   
+sum = numx+numy;
+if (sum>=6)
+{
+    console.log("good")
+}
+else if(sum<6)
+{
+    console.log("not good")
+}
+else{
+    console.log("try again")
+} 
+}
+
+dis(5,4);
